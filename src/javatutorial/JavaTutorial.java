b064d6f22906a74412b7f6f6ca1bbe1b41fa0032
Rishabh <rishabhshah28@gmail.com>
2016-03-08 13:49:27 -0500

    initial commit

---
+++ src/javatutorial/JavaTutorial.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatutorial;
+
+/**
+ *
+ * @author rishabh
+ */
+public class JavaTutorial {
+    
+    public static void main(String[] args) {
+        int noCardsInDeck = 52;
+        System.out.println("Number of cards left: " + noCardsInDeck);
+    }
+}
---
+++ src/javatutorial/MultiplicationTables.java
@@ -0,0 +1,28 @@
+package javatutorial;
+
+public class MultiplicationTables {
+
+    public static void main(String[] args) {
+        for (int n = 1; n <= 12; n++) {
+            int result = 1 * n;
+            System.out.println("1 times " + n + " = " + result);
+        }
+        for (int n = 1; n <= 12; n++) {
+            int result = 2 * n;
+            System.out.println("2 times " + n + " = " + result);
+        }
+        for (int n = 1; n <= 12; n++) {
+            int result = 3 * n;
+            System.out.println("3 times " + n + " = " + result);
+        }
+        for (int n = 1; n <= 12; n++) {
+            int result = 4 * n;
+            System.out.println("4 times " + n + " = " + result);
+        }
+        for (int n = 1; n <= 12; n++) {
+            int result = 5 * n;
+            System.out.println("5 times " + n + " = " + result);
+        }
+    }
+    
+}
