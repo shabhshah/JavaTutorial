acd75963e8a1a9ab6b1d577c0b3e4e64810a5208
Rishabh <rishabhshah28@gmail.com>
2016-03-10 14:08:18 -0500

    put it all into one for loop

--- src/javatutorial/MultiplicationTables.java
+++ src/javatutorial/MultiplicationTables.java
@@ -2,27 +2,12 @@ package javatutorial;
 
 public class MultiplicationTables {
 
-    public static void main(String[] args) {
-        for (int n = 1; n <= 12; n++) {
-            int result = 1 * n;
-            System.out.println("1 times " + n + " = " + result);
-        }
-        for (int n = 1; n <= 12; n++) {
-            int result = 2 * n;
-            System.out.println("2 times " + n + " = " + result);
-        }
-        for (int n = 1; n <= 12; n++) {
-            int result = 3 * n;
-            System.out.println("3 times " + n + " = " + result);
-        }
-        for (int n = 1; n <= 12; n++) {
-            int result = 4 * n;
-            System.out.println("4 times " + n + " = " + result);
-        }
-        for (int n = 1; n <= 12; n++) {
-            int result = 5 * n;
-            System.out.println("5 times " + n + " = " + result);
+    public static void run() {
+        for (int i = 1; i <= 12; i++) {
+            for (int n = 1; n <= 12; n++) {
+                int result = i * n;
+                System.out.println(i + " times " + n + " = " + result);
+            }
         }
     }
-    
 }
