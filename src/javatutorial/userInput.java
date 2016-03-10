b71802e01e9a9247b5a5004b0682161d1ff3b94d
Rishabh <rishabhshah28@gmail.com>
2016-03-10 14:34:21 -0500

    asks for first, middle, and last name and prints it

--- src/javatutorial/userInput.java
+++ src/javatutorial/userInput.java
@@ -1,8 +1,26 @@
 package javatutorial;
 
+import java.util.Scanner;
+
 public class userInput {
     public static void run() {
+        Scanner user_input = new Scanner( System.in);
         
+        String space = " ";
+        
+        String first_name;
+        System.out.print("Enter your first name: ");
+        first_name = user_input.next();
+        
+        String middle_name;
+        System.out.print("Enter your middle name: ");
+        middle_name = user_input.next();
+        
+        String last_name;
+        System.out.print("Enter your last name: ");
+        last_name = user_input.next();
+        
+        System.out.println("\n" + "Welcome " + first_name + space + middle_name + space + last_name);
     }
     
 }
