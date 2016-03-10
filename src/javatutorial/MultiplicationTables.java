abfde18f7e502fcba29ed6706b0f900a2a0b0f73
Rishabh <rishabhshah28@gmail.com>
2016-03-10 14:22:21 -0500

    created formatting for multiplication table

--- src/javatutorial/MultiplicationTables.java
+++ src/javatutorial/MultiplicationTables.java
@@ -6,8 +6,10 @@ public class MultiplicationTables {
         for (int i = 1; i <= 12; i++) {
             for (int n = 1; n <= 12; n++) {
                 int result = i * n;
-                System.out.println(i + " times " + n + " = " + result);
+                System.out.format("%10s %s %s %s %s", ""+(i),"times", (n), "=", (result));
             }
+            
+            System.out.println("/n");
         }
     }
 }
